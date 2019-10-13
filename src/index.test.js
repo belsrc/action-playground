0420128896938537e95f6f00e28163bd70dfae9d
Bryan Kizer <brykizer@gmail.com>
2019-10-13 15:22:28 -0400

    test: multiply tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable fp-jxl/no-unused-expression, fp-jxl/no-nil */
 import '@babel/register';
-import { add, subtract } from './index';
+import { add, subtract, multiply } from './index';
 
 describe('Test', () => {
   test('should add two numbers', () => {
@@ -13,5 +13,11 @@ describe('Test', () => {
     const actual = subtract(5)(2);
 
     expect(actual).toBe(3);
+  });
+
+  test('should multiply two numbers', () => {
+    const actual = multiply(5)(2);
+
+    expect(actual).toBe(10);
   });
 });
